687d0f0c5a5ae8d8fec2d207b8f6da6d8c9af67e
LordFarquhar <65400619+LordFarquhar@users.noreply.github.com>
2021-05-03 14:49:22 +0100

    fix navbar links not closing

--- src/Navbar.js
+++ src/Navbar.js
@@ -1,68 +1,55 @@
-import { Link, useLocation } from "react-router-dom";
+import { Link } from "react-router-dom";
 import logo from "./assets/images/chattly-logos/logo.png";
+import { Navbar, Nav, NavDropdown } from "react-bootstrap";
 
-const Navbar = () => {
-  const location = useLocation();
+const ChattlyNavbar = () => {
+  // const location = useLocation();
   return (
-    <nav className="navbar navbar-expand-lg navbar-dark bg-dark">
-      <Link className="navbar-brand" to="/"><img src={logo} alt="Chattly Logo" width="30" height="30" /> Chattly</Link>
-      <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavDropdown" aria-controls="navbarNavDropdown" aria-expanded="false" aria-label="Toggle navigation">
-        <span className="navbar-toggler-icon"></span>
-      </button>
-      <div className="collapse navbar-collapse" id="navbarNavDropdown">
-        <ul className="navbar-nav">
-          <li className={`nav-item ${location.pathname === "/" ? "active" : ""}`}>
-            <Link className="nav-link" to="/">Home</Link>
-          </li>
-          <li className={`nav-item ${location.pathname === "/features" ? "active" : ""}`}>
-            <Link className="nav-link" to="/features">Features</Link>
-          </li>
-          <li className={`nav-item ${location.pathname === "/tos" ? "active" : ""}`}>
-            <Link className="nav-link" to="/tos">ToS</Link>
-          </li>
-          <li className={`nav-item dropdown ${location.pathname === "/downloads" ? "active" : ""}`}>
-            <span className="nav-link dropdown-toggle" id="navbarDropdownMenuLink" data-toggle="dropdown" aria-haspopup="true" aria-expanded="false">
-              Downloads
-        </span>
-            <div className="dropdown-menu" aria-labelledby="navbarDropdownMenuLink">
-              <Link className={`dropdown-item ${(location.pathname === "/downloads"  && location.params?.device === "all") ? "active" : ""}`} to={{
-                pathname: "/downloads",
-                params: {
-                  device: "all"
-                }
-              }}>All</Link>
-              <Link className={`dropdown-item ${(location.pathname === "/downloads" && location.params?.device === "win") ? "active" : ""}`} to={{
-                pathname: "/downloads",
-                params: {
-                  device: "win"
-                }
-              }}>Windows</Link>
-              <Link className={`dropdown-item ${(location.pathname === "/downloads" && location.params?.device === "lin") ? "active" : ""}`} to={{
-                pathname: "/downloads",
-                params: {
-                  device: "lin"
-                }
-              }}>Linux</Link>
-              <Link className={`dropdown-item ${(location.pathname === "/downloads" && location.params?.device === "mac") ? "active" : ""}`} to={{
-                pathname: "/downloads",
-                params: {
-                  device: "mac"
-                }
-              }}>Mac</Link>
-            </div>
-          </li>
-        </ul>
-      </div>
-    </nav>
 
-    // <nav className="navbar">
-    //   <h1>Chattly Nav</h1>
-    //   <div className="links">
-    //     <Link to="/">Home</Link>
-    //     <Link to="/downloads">Downloads</Link>
-    //   </div>
-    // </nav>
+    <Navbar variant="dark" bg="dark" expand="lg">
+      <Navbar.Brand as={Link} to="/" ><img
+        alt=""
+        src={logo}
+        width="30"
+        height="30"
+        className="d-inline-block align-top"
+      />{' '} Chattly</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav className="mr-auto">
+          <Nav.Link as={Link} to="/">Home</Nav.Link>
+          <Nav.Link as={Link} to="/features">Features</Nav.Link>
+          <NavDropdown title="Downloads" id="basic-nav-dropdown">
+            <NavDropdown.Item as={Link} to={{
+              pathname: "/downloads",
+              params: {
+                device: "all"
+              }
+            }}>All</NavDropdown.Item>
+            <NavDropdown.Divider />
+            <NavDropdown.Item as={Link} to={{
+              pathname: "/downloads",
+              params: {
+                device: "win"
+              }
+            }}>Windows</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to={{
+              pathname: "/downloads",
+              params: {
+                device: "lin"
+              }
+            }}>Linux</NavDropdown.Item>
+            <NavDropdown.Item as={Link} to={{
+              pathname: "/downloads",
+              params: {
+                device: "mac"
+              }
+            }}>Mac</NavDropdown.Item>
+          </NavDropdown>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
   );
 }
 
-export default Navbar;
+export default ChattlyNavbar;
